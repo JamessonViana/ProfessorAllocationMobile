96439112236a83f617e5bdb05ae99ae24315f724
NairtonMenezes <nairtonmenezesjunior@gmail.com>
2024-08-06 22:12:19 -0300

    Class Professor e ProfessorService

---
+++ app/src/main/java/com/example/professorallocation/Main/Models/Professor.kt
@@ -0,0 +1,6 @@
+package com.example.professorallocation.Main.Models
+
+data class Professor(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/professorallocation/Main/Service/ProfessorService.kt
@@ -0,0 +1,37 @@
+package com.example.professorallocation.Main.Service
+
+import com.example.professorallocation.Main.Models.Professor
+import com.example.professorallocation.Main.Repository.RetrofitConfig
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ProfessorService {
+
+    @GET("professor")
+    fun getProfessor() : Call<List<Professor>>
+
+    @POST("professor")
+    fun postProfessor(@Body curso: Professor) : Call<Any>
+
+    @GET("professor/{id}")
+    fun getProfessor(@Path("id") cursoId: Int) : Call<Professor>
+
+    @PUT("professor/{id}")
+    fun putProfessor(@Path("id") cursoId: Int, @Body curso: Professor) : Call<Professor>
+
+    @DELETE("professor/{id}")
+    fun deleteProfessor(@Path("id") cursoId: Int) : Call<Any>
+
+    companion object {
+        fun create(): ProfessorService {
+            val retrofit = RetrofitConfig.retrofit
+            return retrofit.create(ProfessorService::class.java)
+        }
+    }
+
+}
